4dbb657a6ec2e8a1e31e0ae7d3aec0bdfac973da
JJLim <lim.jess9@gmail.com>
2015-01-20 16:31:30 -0500

    Check if bananas is in the list and outputs whether or not to pick it up

--- exercise6.rb
+++ exercise6.rb
@@ -11,3 +11,11 @@ grocery_list << "rice"
 list(grocery_list)
 
 puts grocery_list.length
+
+#puts grocery_list.include?("bananas") "You need to pick up bananas" : "You don't need to pick up bananas today"
+
+if grocery_list.include?("bananas")
+	puts "You need to pick up bananas"
+else
+	puts "You don't need to pick up bananas today"
+end
