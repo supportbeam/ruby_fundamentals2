ecde1b494cd003d255a39492b7af3242411fb393
JJLim <lim.jess9@gmail.com>
2015-01-20 17:06:42 -0500

    Added key cohort4 with a value of 43

--- exercise7.rb
+++ exercise7.rb
@@ -1,12 +1,18 @@
+# create a hash of the cohorts
 students = {
 	:cohort1 => 34,
 	:cohort2 => 42,
 	:cohort3 => 22
 }
 
+# define a method that displays the cohort and number of students
 def cohorts(s)
 	s.each { |key, value| puts "#{key}: #{value} students" }
 end
 
+# outputs cohort names and the amount of students
 cohorts(students)
+
+# Added another key-value pair
+students[:cohort4] = 43
 
